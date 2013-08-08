1f4edcb3dad2426714a90bb9f611e9cd477a89e7
Kris Jenkins <krisajenkins@gmail.com>
2013-08-08 15:56:41 +0100

    That's the first half of the sheet converted into a hierarchy. Phew!

--- clojure-cheatsheet.el
+++ clojure-cheatsheet.el
@@ -322,315 +322,94 @@
 	  ("From Other"
 	   (clojure.core file-seq line-seq resultset-seq re-seq tree-seq xml-seq iterator-seq enumeration-seq))
 	  ("From Seq"
-	   (clojure.core keep keep-indexed))))
+	   (clojure.core keep keep-indexed)))
 
+	 ("Seq in, Seq out"
+	  ("Get shorter"
+	   (clojure.core distinct filter remove for))
+	  ("Get longer"
+	   (clojure.core cons conj concat lazy-cat mapcat cycle interleave interpose)))
+	 ("Tail-items"
+	  (clojure.core rest nthrest fnext nnext drop drop-while take-last for))
+	 ("Head-items"
+	  (clojure.core take take-nth take-while butlast drop-last for))
+	 ("'Change'"
+	  (clojure.core conj concat distinct flatten group-by partition partition-all partition-by split-at split-with filter remove replace shuffle))
+	 ("Rearrange"
+	  (clojure.core reverse sort sort-by compare))
+	 ("Process items"
+	  (clojure.core map pmap map-indexed mapcat for replace seque))
+
+	 ("Using a Seq"
+	  ("Extract item"
+	   (clojure.core first second last rest next ffirst nfirst fnext nnext nth nthnext rand-nth when-first max-key min-key))
+	  ("Construct coll"
+	   (clojure.core zipmap into reduce reductions set vec into-array to-array-2d))
+	  ("Pass to fn"
+	   (clojure.core apply))
+	  ("Search"
+	   (clojure.core some filter))
+	  ("Force evaluation"
+	   (clojure.core doseq dorun doall))
+	  ("Check for forced"
+	   (clojure.core realized?))))
+
+	("Zippers"
+	 ("Create"	
+	  (clojure.zip zipper seq-zip vector-zip xml-zip))
+	 ("Get loc"	
+	  (clojure.zip up down left right leftmost rightmost))
+	 ("Get seq"	
+	  (clojure.zip lefts rights path children))
+	 ("'Change'"	
+	  (clojure.zip make-node replace edit insert-child insert-left insert-right append-child remove))
+	 ("Move"	
+	  (clojure.zip next prev))
+	 ("Misc"	
+	  (clojure.zip root node branch? end?)))
+
+	("Documentation"
+	 ("REPL"
+	  (clojure.repl doc find-doc apropos source pst javadoc )))
+
+	("Transients"
+	 ("Create")
+	 (clojure.core transient persistent!)
+	 ("Change")
+	 (clojure.core conj! pop! assoc! dissoc! disj! Note: always use return value for later changes, never original!))
+	("Misc"
+	 ("Compare"
+	  (clojure.core = == identical? not= not compare)
+	  (clojure.data diff))
+	 ("Test"
+	  (clojure.core true? false? nil? instance?)))
+
+
+	("IO"
+	 ("to/from ..."
+	  (clojure.core spit slurp))
+	 ("to *out*"
+	  (clojure.core pr prn print printf println newline)
+	  (clojure.pprint print-table))
+	 ("to writer"
+	  (clojure.pprint pprint cl-format))
+	 ("to string"
+	  (clojure.core format with-out-str pr-str prn-str print-str println-str))
+	 ("from *in*"
+	  (clojure.core read-line read))
+	 ("from reader"
+	  (clojure.core line-seq read)) 
+	 ("from string"
+	  (clojure.core read-string with-in-str))
+	 ("Open"
+	  (clojure.core with-open)
+	  (clojure.java.io  reader writer input-stream output-stream))
+	 ("Misc"
+	  (clojure.core flush file-seq *in* *out* *err*)
+	  (clojure.java.io file copy delete-file resource as-file as-url as-relative-path))) 
 	
-
 	))))))
 
-
-;; (("Unfiled") 
-
-;;  clojure.core/*allow-unresolved-vars*
-;;  clojure.core/*assert*
-;;  clojure.core/*clojure-version*
-;;  clojure.core/*command-line-args*
-;;  clojure.core/*compile-files*
-;;  clojure.core/*compile-path*
-;;  clojure.core/*compiler-options*
-;;  clojure.core/*err*
-;;  clojure.core/*file*
-;;  clojure.core/*flush-on-newline*
-;;  clojure.core/*fn-loader*
-;;  clojure.core/*in*
-;;  clojure.core/*math-context*
-;;  clojure.core/*out*
-;;  clojure.core/*read-eval*
-;;  clojure.core/*source-path*
-;;  clojure.core/*use-context-classloader*
-;;  clojure.core/*verbose-defrecords*
-;;  clojure.core/*warn-on-reflection*
-;;  clojure.core/->ArrayChunk
-;;  clojure.core/->Vec
-;;  clojure.core/->VecNode
-;;  clojure.core/->VecSeq
-;;  clojure.core/-cache-protocol-fn
-;;  clojure.core/-reset-methods
-;;  clojure.core/..
-;;  clojure.core/EMPTY-NODE
-;;  clojure.core/accessor
-;;  clojure.core/aclone
-;;  clojure.core/add-classpath
-;;  clojure.core/agent-errors
-;;  clojure.core/aget
-;;  clojure.core/alength
-;;  clojure.core/alter-meta!
-;;  clojure.core/alter-var-root
-;;  clojure.core/amap
-;;  clojure.core/ancestors
-;;  clojure.core/and
-;;  clojure.core/apply
-;;  clojure.core/areduce
-;;  clojure.core/aset
-;;  clojure.core/aset-boolean
-;;  clojure.core/aset-byte
-;;  clojure.core/aset-char
-;;  clojure.core/aset-double
-;;  clojure.core/aset-float
-;;  clojure.core/aset-int
-;;  clojure.core/aset-long
-;;  clojure.core/aset-short
-;;  clojure.core/assert
-;;  clojure.core/assoc!
-;;  clojure.core/await1
-;;  clojure.core/bases
-;;  clojure.core/binding
-;;  clojure.core/boolean
-;;  clojure.core/boolean-array
-;;  clojure.core/booleans
-;;  clojure.core/bound?
-;;  clojure.core/butlast
-;;  clojure.core/byte-array
-;;  clojure.core/bytes
-;;  clojure.core/case
-;;  clojure.core/cast
-;;  clojure.core/char-array
-;;  clojure.core/chars
-;;  clojure.core/chunk
-;;  clojure.core/chunk-append
-;;  clojure.core/chunk-buffer
-;;  clojure.core/chunk-cons
-;;  clojure.core/chunk-first
-;;  clojure.core/chunk-next
-;;  clojure.core/chunk-rest
-;;  clojure.core/chunked-seq?
-;;  clojure.core/class
-;;  clojure.core/class?
-;;  clojure.core/clear-agent-errors
-;;  clojure.core/clojure-version
-;;  clojure.core/comment
-;;  clojure.core/comparator
-;;  clojure.core/compile
-;;  clojure.core/concat
-;;  clojure.core/cond
-;;  clojure.core/condp
-;;  clojure.core/conj!
-;;  clojure.core/construct-proxy
-;;  clojure.core/create-struct
-;;  clojure.core/cycle
-;;  clojure.core/decimal?
-;;  clojure.core/declare
-;;  clojure.core/default-data-readers
-;;  clojure.core/definterface
-;;  clojure.core/defmacro
-;;  clojure.core/defmethod
-;;  clojure.core/defmulti
-;;  clojure.core/defonce
-;;  clojure.core/defprotocol
-;;  clojure.core/defrecord
-;;  clojure.core/defstruct
-;;  clojure.core/deftype
-;;  clojure.core/delay
-;;  clojure.core/delay?
-;;  clojure.core/denominator
-;;  clojure.core/derive
-;;  clojure.core/descendants
-;;  clojure.core/destructure
-;;  clojure.core/disj
-;;  clojure.core/disj!
-;;  clojure.core/dissoc
-;;  clojure.core/dissoc!
-;;  clojure.core/distinct
-;;  clojure.core/doall
-;;  clojure.core/dorun
-;;  clojure.core/doseq
-;;  clojure.core/dotimes
-;;  clojure.core/doto
-;;  clojure.core/double-array
-;;  clojure.core/doubles
-;;  clojure.core/drop
-;;  clojure.core/drop-last
-;;  clojure.core/drop-while
-;;  clojure.core/eval
-;;  clojure.core/ex-data
-;;  clojure.core/ex-info
-;;  clojure.core/extend
-;;  clojure.core/extend-protocol
-;;  clojure.core/extend-type
-;;  clojure.core/extenders
-;;  clojure.core/extends?
-;;  clojure.core/false?
-;;  clojure.core/ffirst
-;;  clojure.core/filter
-;;  clojure.core/find-protocol-impl
-;;  clojure.core/find-protocol-method
-;;  clojure.core/find-var
-;;  clojure.core/flatten
-;;  clojure.core/float-array
-;;  clojure.core/float?
-;;  clojure.core/floats
-;;  clojure.core/flush
-;;  clojure.core/fnext
-;;  clojure.core/for
-;;  clojure.core/force
-;;  clojure.core/gen-class
-;;  clojure.core/gen-interface
-;;  clojure.core/get-method
-;;  clojure.core/get-proxy-class
-;;  clojure.core/hash
-;;  clojure.core/hash-combine
-;;  clojure.core/if-let
-;;  clojure.core/if-not
-;;  clojure.core/init-proxy
-;;  clojure.core/instance?
-;;  clojure.core/int-array
-;;  clojure.core/integer?
-;;  clojure.core/interleave
-;;  clojure.core/interpose
-;;  clojure.core/into-array
-;;  clojure.core/ints
-;;  clojure.core/isa?
-;;  clojure.core/last
-;;  clojure.core/lazy-cat
-;;  clojure.core/let
-;;  clojure.core/letfn
-;;  clojure.core/long-array
-;;  clojure.core/longs
-;;  clojure.core/loop
-;;  clojure.core/macroexpand
-;;  clojure.core/macroexpand-1
-;;  clojure.core/make-array
-;;  clojure.core/make-hierarchy
-;;  clojure.core/map
-;;  clojure.core/map-indexed
-;;  clojure.core/mapcat
-;;  clojure.core/max-key
-;;  clojure.core/meta 
-;;  clojure.core/method-sig
-;;  clojure.core/methods
-;;  clojure.core/min-key
-;;  clojure.core/munge
-;;  clojure.core/name
-;;  clojure.core/namespace-munge
-;;  clojure.core/newline
-;;  clojure.core/next
-;;  clojure.core/nfirst
-;;  clojure.core/nnext
-;;  clojure.core/not
-;;  clojure.core/nthnext
-;;  clojure.core/nthrest
-;;  clojure.core/number?
-;;  clojure.core/numerator
-;;  clojure.core/object-array
-;;  clojure.core/or
-;;  clojure.core/parents
-;;  clojure.core/partition
-;;  clojure.core/partition-all
-;;  clojure.core/partition-by
-;;  clojure.core/persistent!
-;;  clojure.core/pop!
-;;  clojure.core/pr
-;;  clojure.core/pr-str
-;;  clojure.core/prefer-method
-;;  clojure.core/prefers
-;;  clojure.core/primitives-classnames
-;;  clojure.core/print
-;;  clojure.core/print-ctor
-;;  clojure.core/print-dup
-;;  clojure.core/print-method
-;;  clojure.core/print-simple
-;;  clojure.core/print-str
-;;  clojure.core/printf
-;;  clojure.core/println
-;;  clojure.core/println-str
-;;  clojure.core/prn
-;;  clojure.core/prn-str
-;;  clojure.core/proxy
-;;  clojure.core/proxy-call-with-super
-;;  clojure.core/proxy-mappings
-;;  clojure.core/proxy-name
-;;  clojure.core/proxy-super
-;;  clojure.core/rand-nth
-;;  clojure.core/ratio?
-;;  clojure.core/rational?
-;;  clojure.core/read
-;;  clojure.core/read-line
-;;  clojure.core/read-string
-;;  clojure.core/realized?
-;;  clojure.core/reduce
-;;  clojure.core/reduced
-;;  clojure.core/reduced?
-;;  clojure.core/reductions
-;;  clojure.core/reify
-;;  clojure.core/remove
-;;  clojure.core/remove-
-;;  clojure.core/remove-method
-;;  clojure.core/replicate
-;;  clojure.core/reset-meta!
-;;  clojure.core/rest
-;;  clojure.core/reverse
-;;  clojure.core/satisfies?
-;;  clojure.core/second
-;;  clojure.core/send-via
-;;  clojure.core/sequence
-;;  clojure.core/set-agent-send-executor!
-;;  clojure.core/set-agent-send-off-executor!
-;;  clojure.core/short-array
-;;  clojure.core/shorts
-;;  clojure.core/shuffle
-;;  clojure.core/slurp
-;;  clojure.core/sort
-;;  clojure.core/sort-by
-;;  clojure.core/special-symbol?
-;;  clojure.core/spit
-;;  clojure.core/split-at
-;;  clojure.core/split-with
-;;  clojure.core/struct
-;;  clojure.core/struct-map
-;;  clojure.core/supers
-;;  clojure.core/take
-;;  clojure.core/take-last
-;;  clojure.core/take-nth
-;;  clojure.core/take-while
-;;  clojure.core/test
-;;  clojure.core/the-ns
-;;  clojure.core/thread-bound?
-;;  clojure.core/time
-;;  clojure.core/to-array
-;;  clojure.core/to-array-2d
-;;  clojure.core/trampoline
-;;  clojure.core/transient
-;;  clojure.core/true?
-;;  clojure.core/type
-;;  clojure.core/underive
-;;  clojure.core/unquote
-;;  clojure.core/unquote-splicing
-;;  clojure.core/update-proxy
-;;  clojure.core/var-get
-;;  clojure.core/var-set
-;;  clojure.core/var?
-;;  clojure.core/vary-meta
-;;  clojure.core/vec
-;;  clojure.core/vector
-;;  clojure.core/vector-of
-;;  clojure.core/when
-;;  clojure.core/when-first
-;;  clojure.core/when-let
-;;  clojure.core/when-not
-;;  clojure.core/while
-;;  clojure.core/with-bindings
-;;  clojure.core/with-bindings*
-;;  clojure.core/with-in-str
-;;  clojure.core/with-loading-context
-;;  clojure.core/with-local-vars
-;;  clojure.core/with-meta
-;;  clojure.core/with-open
-;;  clojure.core/with-out-str
-;;  clojure.core/with-redefs
-;;  clojure.core/with-redefs-fn
-;;  clojure.walk/keywordize-keys
-;;  clojure.walk/macroexpand-all
-;;  clojure.walk/stringify-keys)
 
 ;;;###autoload
 (defun clojure-cheatsheet ()
