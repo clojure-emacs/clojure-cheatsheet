4096744bdfb8bc2249d893b5574b068ca7a42b24
Kris Jenkins <krisajenkins@gmail.com>
2013-08-08 17:55:19 +0100

    Removing the autoloads file. I shouldn't have committed it.

--- clojure-cheatsheet-autoloads.el
+++
@@ -1,28 +0,0 @@
-;;; clojure-cheatsheet-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads (clojure-cheatsheet) "clojure-cheatsheet" "clojure-cheatsheet.el"
-;;;;;;  (20995 52265 0 0))
-;;; Generated autoloads from clojure-cheatsheet.el
-
-(autoload 'clojure-cheatsheet "clojure-cheatsheet" "\
-Use helm to show a Clojure cheatsheet.
-
-\(fn)" t nil)
-
-;;;***
-
-;;;### (autoloads nil nil ("support.el") (20995 52271 420071 0))
-
-;;;***
-
-(provide 'clojure-cheatsheet-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; clojure-cheatsheet-autoloads.el ends here
