d44a226b38a8b83d75e4f36432bbfdcfeb7de0ac
Kris Jenkins <krisajenkins@gmail.com>
2016-07-07 09:12:47 +0100

    Updates Clojure dependencies.

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject clojure-cheatsheet "0.1.0"
   :description "Dependencies for clojure-cheatsheet"
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/data.zip "0.1.1"]
-                 [org.clojure/core.async "0.2.374"]])
+                 [org.clojure/data.zip "0.1.2"]
+                 [org.clojure/core.async "0.2.385"]])
